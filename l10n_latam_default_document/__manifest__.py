4791af60ff2eea2393796bd41ae000d3d650f1e0
Daniel Blanco <daniel@blancomartin.cl>
2024-12-07 09:30:05 -0300

    [FIX] fix version convention for bmya

--- l10n_latam_default_document/__manifest__.py
+++ l10n_latam_default_document/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Default Latam Document',
-    'version': '18.0.1.0',
+    'version': '18.0.1.0.0',
     'summary': 'Default Latam Document',
     'description': 'Default Latam Document',
     'category': 'Localization',
